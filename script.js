e9e28472a5067da1760b840067ae09eac1bd3932
Mike Gazzaruso <mike@gazzaruso.com>
2022-06-11 00:52:50 +0200

    Implement Octave Switching

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var ctx = null;
 var osc = null;
 
+let oscType = 'sine';
+let octave = 1;
+
 const keys = document.querySelectorAll('.key');
 const startAudioButton = document.querySelector('.startAudio');
+const sineWaveButton = document.querySelector('.sinewavetype');
+const sawWaveButton = document.querySelector('.sawwavetype');
+const octMinusOne = document.querySelector('.octminusone');
+const octEven = document.querySelector('.octeven');
+const octPlusOne = document.querySelector('.octplusone');
 
 const freqs = {
     do: 261.626,
@@ -27,8 +35,8 @@ function playSound(key) {
         osc = ctx.createOscillator();
     }
 
-        osc.type = "sine";
-        osc.frequency.value = freqs[key];
+        osc.type = oscType;
+        osc.frequency.value = freqs[key] * octave;
         osc.start(0);
         osc.connect(ctx.destination);
 }
@@ -64,5 +72,40 @@ startAudioButton.addEventListener('click', function() {
     if (ctx === null) {
        ctx = new AudioContext({latencyHint: 'interactive'});
        ctx.resume();
+       startAudioButton.classList.add('started');
+       startAudioButton.textContent = ('____OK____ ');
     }
-})
+});
+
+sineWaveButton.addEventListener('click', function() {
+    oscType = 'sine';
+
+    sineWaveButton.querySelector('img').classList.add('selected');
+    sineWaveButton.querySelector('img').classList.remove('unselected');
+
+    sawWaveButton.querySelector('img').classList.add('unselected');
+    sawWaveButton.querySelector('img').classList.remove('selected');
+});
+
+sawWaveButton.addEventListener('click', function() {
+    oscType = 'sawtooth';
+
+    sawWaveButton.querySelector('img').classList.add('selected');
+    sawWaveButton.querySelector('img').classList.remove('unselected');
+
+    sineWaveButton.querySelector('img').classList.add('unselected');
+    sineWaveButton.querySelector('img').classList.remove('selected');
+});
+
+octMinusOne.addEventListener('click', function() {
+    octave = Math.max(0.125, octave * 0.5);
+});
+
+octEven.addEventListener('click', function() {
+    octave = 1;
+});
+
+octPlusOne.addEventListener('click', function() {
+    octave = Math.min(8, octave * 2);
+});
+
